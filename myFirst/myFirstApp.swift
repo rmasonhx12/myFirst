27e92d4360256de36d19f7cc21e9d8352eb9cd84
Robert Hendricks <rmasonh19@gmail.com>
2024-08-18 00:13:36 -0400

    Initial Commit

---
+++ myFirst/myFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstApp.swift
+//  myFirst
+//
+//  Created by Robert Hendricks on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct myFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
